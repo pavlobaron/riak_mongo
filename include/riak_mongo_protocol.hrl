10cc1515d4cb7dcd075ac0ae52dd718c84a5ac59
Kresten Krab Thorup <krab@trifork.com>
2012-04-11 00:05:30 +0200

    Module for decomposing mongo wire messages

---
+++ include/riak_mongo_protocol.hrl
@@ -0,0 +1,60 @@
+
+-record (mongo_insert, {
+           request_id :: integer(),
+           dbcoll :: binary(),
+           continueonerror = false :: boolean(),
+           documents :: [bson:document()] }).
+
+-record (mongo_update, {
+           request_id :: integer(),
+           dbcoll :: binary(),
+           upsert = false :: boolean(),
+           multiupdate = false :: boolean(),
+           selector :: bson:document(),
+           updater :: bson:document()  }).
+
+-record (mongo_delete, {
+           request_id :: integer(),
+           dbcoll :: binary(),
+           singleremove = false :: boolean(),
+           selector :: bson:document() }).
+
+-record (mongo_killcursor, {
+           request_id :: integer(),
+           cursorids :: [integer()] }).
+
+-record (mongo_query, {
+           request_id :: integer(),
+           dbcoll :: binary(),
+
+           tailablecursor = false :: boolean(),
+           slaveok = false :: boolean(),
+           nocursortimeout = false :: boolean(),
+           awaitdata = false :: boolean(),
+           exhaust = false :: boolean(),
+           oplogreplay = false :: boolean(),
+           partial = false :: boolean(),
+
+           skip = 0 :: integer(),
+           batchsize = 0 :: integer(), %% negative closes cursor
+           selector :: bson:document(),
+           projector = [] :: bson:document() }).
+
+-record (mongo_getmore, {
+           request_id :: integer(),
+           dbcoll :: binary(),
+           batchsize = 0 :: integer(),
+           cursorid :: integer() }).
+
+-record (mongo_reply, {
+           reply_to :: integer(),
+           cursornotfound :: boolean(),
+           queryerror :: boolean(),
+           awaitcapable :: boolean(),
+           cursorid :: integer(),
+           startingfrom :: integer(),
+           documents :: [bson:document()] }).
+
+-type mongo_message() :: #mongo_insert{} | #mongo_update{} | #mongo_delete{}
+                         | #mongo_killcursor{} | #mongo_query{} | #mongo_getmore{}.
+
