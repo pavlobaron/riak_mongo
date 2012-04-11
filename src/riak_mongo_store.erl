4d8f5fea494169796f1fb3ba3c56cc8a6fa811b1
pavlobaron <pb@pbit.org>
2012-04-11 13:40:45 +0200

    replSetGetStatus for shell, some restructure work

---
+++ src/riak_mongo_store.erl
@@ -0,0 +1,43 @@
+%%
+%% This file is part of riak_mongo
+%%
+%% Copyright (c) 2012 by Pavlo Baron (pb at pbit dot org)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
+%% @author Pavlo Baron <pb at pbit dot org>
+%% @doc Here we speak to the Riak store
+%% @copyright 2012 Pavlo Baron
+
+-module(riak_mongo_store).
+
+%-export([insert/1, find/1]).
+
+%insert(#mongo_insert{dbcol=Collection, documents=Doc}) ->
+%    {ok, C} = riak:local_client(),
+%    O = riak_object:new(Collection, ID, Doc),
+%    C:put(O).
+
+%find(#mongo_query=Query) ->
+    % query!!!
+    % if NumberToReturn < 0 then we close the cursor (cursor? state?)
+%    {ok, C} = riak:local_client(),
+%    {ok, L} = C:list_keys(Collection),
+%    collect_objects(C, L).
+
+%collect_objects(_, []) ->
+%    [];
+%collect_objects(C, [Key|L]) ->
+%    {ok, O} = C:get(Collection, Key),
+%    [riak_object:get_value(O)|collect_objects(C, L)].
